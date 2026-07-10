b36f224b995c68a31e059864e7efa20b14b99fa5
tiagorsdfonseca <tiagorsdfonseca@gmail.com>
2026-07-10 22:36:17 +0100

    Added testcontainers logic to the orderApplicationTests

--- order/src/test/java/com/example/order/OrderApplicationTests.java
+++ order/src/test/java/com/example/order/OrderApplicationTests.java
@@ -8,16 +8,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
+
 @SpringBootTest
 //@EnableAutoConfiguration(exclude = { SimpleDiscoveryClientAutoConfiguration.class })
 /*@ImportAutoConfiguration(exclude ={
 	org.springframework.cloud.autoconfigure.RefreshAutoConfiguration.class
 })*/
 @TestPropertySource(properties = "catalog.service.url=http://localhost:8082")
+@ActiveProfiles("test")
+@Testcontainers
 class OrderApplicationTests {
 
 	@MockitoBean
 	private KafkaTemplate<String, ?> kafkaTemplate;
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
 	@Test
 	void contextLoads() {
